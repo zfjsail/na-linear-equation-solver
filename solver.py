5d4478794515d1f20da03bf49fafe806bfb1dc94
Fanjin Zhang <zfjsail@163.com>
2018-05-17 22:36:17 +0800

    jacobi gaussian seidel right?

--- solver.py
+++ solver.py
@@ -5,6 +5,7 @@ from scipy.linalg import hilbert
 class Solver:
     def __init__(self, A, b):
         self.A = A  # square
+        print(self.A)
         self.b = b
         self.n = A.shape[0]
         self.x = np.empty(self.n)
@@ -27,10 +28,45 @@ class Solver:
         print('--------gaussian elimination--------')
         print(self.x)
 
+    def jacobi_iter(self, N=25, x=None):
+        if x is None:
+            x = np.zeros((self.n, 1))
+
+        D = np.diag(self.A)
+        D_1 = np.diagflat(1 / D)
+        L_plus_U = np.diagflat(D) - self.A
+
+        for i in range(N):
+            x = np.dot(D_1, np.dot(L_plus_U, x)) + np.dot(D_1, self.b)
+
+        print('------jacobi--------')
+        print(x)
+
+    def gauss_seidel(self, N=25, x=None):
+        if x is None:
+            x = np.zeros((self.n, 1))
+
+        # U = np.triu(self.A, 1)
+        # D_minus_L = np.tril(self.A)
+
+        for i in range(N):
+            for j in range(self.n):
+                r = self.b[j]
+                for k in range(j):
+                    r -= self.A[j, k] * x[k]
+                for k in range(j+1, self.n):
+                    r -= self.A[j, k] * x[k]
+                x[j] = r / self.A[j, j]
+            print('remain', np.dot(self.A, x) - self.b)
+        print('--------gauss seidel-------')
+        print(x)
+
 
 if __name__ == '__main__':
-    d = 4
+    d = 2
     a = hilbert(d)
     x = np.ones((d, 1))
     solver = Solver(a, np.dot(a, x))
-    solver.gaussian_elimination()
+    # solver.gaussian_elimination()
+    # solver.jacobi_iter()
+    solver.gauss_seidel()
